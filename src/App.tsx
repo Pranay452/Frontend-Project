7b7eadabf47c0381e260a587ad458a5966117a8a
Pranay Kumar <manepallypranay98@gmail.com>
2024-07-13 22:10:07 +0530

    first push

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+        <div className="bg-white p-8 rounded shadow-lg">
+          <h1 className="text-2xl font-bold mb-4">Hello, Tailwind CSS!</h1>
+          <p className="text-gray-700">
+            Welcome to your TypeScript + Tailwind CSS React App.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
